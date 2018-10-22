fb9a9d1aefcbcbb0497e0c6a1e24a128fb6f97f8
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-10-22 16:40:10 -0500

    Damien: Modifications

--- login_script.php
+++ login_script.php
@@ -1,15 +1,34 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost","root","","CUSTOMER");
 	
-$message="";
-if(!empty($_POST["login"])) {
-	$result = mysqli_query($conn,"SELECT * FROM users WHERE user_name='" . $_POST["user_name"] . "' and password = '". $_POST["password"]."'");
-	$row  = mysqli_fetch_array($result);
-	if(is_array($row)) {
-	$_SESSION["user_id"] = $row['user_id'];
-	} else {
-	$message = "Invalid Username or Password!";
+$error='';
+if (isset($_POST['submit'])){
+	if((empty($_POST['username'])) || empty($_POST['password'])){
+		$error = "Usernane or Password is invalid.";
+	}
+	else{
+		$user=$_POST['username'];
+		$pass=$_POST['password'];
+
+		$conn = mysqli_connect("localhost","root","");
+		
+		//Protection from SQL injection
+		$user=stripslashes($user);
+		$pass=stripslashes($pass);
+		$user=mysqli_real_escape_string($user);
+		$pass=mysqli_real_escape_string($pass);
+
+		$db=mysqli_select_db("CUSTOMER",$conn);
+
+		$query=mysqli_query("select * from CUSTOMER where Password='$pass' and username='$user'",$conn);
+		$rows=mysqli_num_rows($query);
+		if($rows==1){
+			$_SESSION['login_user']=$user;
+			header("location: HomePage.html");
+		}else{
+			$error = "Username or Password is invalid.";
+		}
+		mysqli_close($conn);
 	}
 }
 ?>
