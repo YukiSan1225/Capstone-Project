a98037c583c764cfd71bdab32a0656c9f8ae06b6
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-10-22 16:49:11 -0500

    Damien: Reorg. the repo

--- login_script.php
+++ login_script.php
@@ -1,34 +1,12 @@
 <?php
-session_start();
-	
-$error='';
-if (isset($_POST['submit'])){
-	if((empty($_POST['username'])) || empty($_POST['password'])){
-		$error = "Usernane or Password is invalid.";
-	}
-	else{
-		$user=$_POST['username'];
-		$pass=$_POST['password'];
-
-		$conn = mysqli_connect("localhost","root","");
-		
-		//Protection from SQL injection
-		$user=stripslashes($user);
-		$pass=stripslashes($pass);
-		$user=mysqli_real_escape_string($user);
-		$pass=mysqli_real_escape_string($pass);
-
-		$db=mysqli_select_db("CUSTOMER",$conn);
-
-		$query=mysqli_query("select * from CUSTOMER where Password='$pass' and username='$user'",$conn);
-		$rows=mysqli_num_rows($query);
-		if($rows==1){
-			$_SESSION['login_user']=$user;
-			header("location: HomePage.html");
-		}else{
-			$error = "Username or Password is invalid.";
-		}
-		mysqli_close($conn);
-	}
+$dbh = mysqli_connect('localhost', 'admin', 'pass');
+if (!$dbh) {
+    die('Could not connect: ' . mysqli_error());
 }
+echo 'Connected successfully to MariaDB database';
+mysqli_close($dbh);
+?>
+
+<?php
+phpinfo();
 ?>
---
+++ session.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysqli_connect("localhost","root","");
+
+$db = mysqli_select_db("CUSTOMER", $conn);
+
+session_start();
+
+$user=$_SESSION['login_user'];
+
+$sqlsession = mysqli_query("select username from CUSTOMER where username='$user'", $conn);
+$row=mysqli_fetch_assoc($sqlsession);
+$login_session=$row['username'];
+if(!isset($login_session)){
+    mysqli_close($conn);
+    header('Location: index.php');
+}
+?>
