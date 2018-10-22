21f10dc5be9375c096c2b34777856163bd8a431c
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-10-22 17:03:37 -0500

    Missing value in DB

--- customertable.sql
+++ customertable.sql
@@ -5,6 +5,7 @@ customerID int (8),
  FName varchar(30) NOT NULL,
  Email_Address varchar(30) NOT NULL,
  Phone char(10) NOT NULL,
+ username varchar(30) NOT NULL,
  Password varchar (30) NOT NULL,
  primary key (customerID)
  );
