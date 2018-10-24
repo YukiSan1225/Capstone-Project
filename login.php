aa96c0fb6ce8589dbd6e5bfa34952cf3eba046dc
Damien Burks <dburksgtr@gmail.com>
2018-10-24 09:13:58 -0500

    Damien: Changes

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
       </div>
     </header>
 
-    <form action="login_script" method="post"> <!--Need to create this file-->
+    <form action="login_script.php" method="post"> <!--Need to create this file-->
         Email:<br>
         <input type="text" name="email" placeholder="email" type="text"><br>
         Password:<br>
