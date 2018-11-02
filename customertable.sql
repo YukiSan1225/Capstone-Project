b47b694de8b6cb16ca9b8b87457d5a2152b7d670
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-11-02 12:42:31 -0500

    Merging files

--- customertable.sql
+++ customertable.sql
@@ -1,12 +1,22 @@
 
-CREATE TABLE CUSTOMER (
-customerID int (8),
- LName varchar(30) NOT NULL,
+CREATE TABLE cuslogin (
+LName varchar(30) NOT NULL,
  FName varchar(30) NOT NULL,
- Email_Address varchar(30) NOT NULL,
+ Email_Address varchar(50) NOT NULL,
  Phone char(10) NOT NULL,
- username varchar(30) NOT NULL,
  Password varchar (30) NOT NULL,
- primary key (customerID)
+ primary key (Email_Address)
  );
 
+INSERT INTO CUSLOGIN (Lname, FName, Email_Address, Phone, Password) VALUES ("Burks","Damien","dburksgtr@gmail.com","8320901324","IHateYou");
+INSERT INTO CUSLOGIN (Lname, FName, Email_Address, Phone, Password) VALUES ("Johnson","Maria","mariadb@gmail.com","7810392456","ILoveYou");
+
+CREATE TABLE cushome (
+website_name varchar(50) NOT NULL,
+    email_address varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+    CONSTRAINT `fk_cuslogin_email`
+        FOREIGN KEY (email_address) REFERENCES cuslogin (Email_Address)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT
+);
--- login.php
+++ login.php
@@ -23,7 +23,7 @@ header("location: LoginHomePage.php");
           <ul>
             <li class="current"><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="ContactPage.html">Contact</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
           </ul>
