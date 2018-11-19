0d2db4d47d67857d5f2bf39489752423e43352ed
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-11-19 16:21:44 -0600

    Changes to homepage

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ include('connect.php');
 
 if(isset($_GET['tnum']) && is_numeric($_GET['tnum'])){
     $id = $_GET['tnum'];
-    $result = mysqli_query($con, "delete from cushome where id='".$id."'") or die(mysqli_error());
+    $result = mysqli_query($con, "delete from cushome where tnum='".$id."'") or die(mysqli_error());
 
     header("Location: homepage.php");
 }
