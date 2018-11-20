c05e1cd8a5bd04dc75aff77d00735012b211c1ee
Damien Burks <dburksgtr@gmail.com>
2018-11-20 00:46:38 -0600

    Adding to homepage

--- add.php
+++ add.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 if(isset($_SESSION['usr_id'])!="") {
     header("Location: index.php");
@@ -14,7 +13,7 @@ if (isset($_POST['add'])) {
     $url = mysqli_real_escape_string($con, $_POST['url']);
     $email_username = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
-    $usrid = $_GET['id'];
+    $usrid = $_POST['id'];
 
     if (!$error) {
         if(mysqli_query($con, "INSERT INTO cushome (website_name, cusid, password, email_username) VALUES ('" . $url . "', '" . $usrid . "', '" . $password . "', '" . $email_username . "', '" . $password . "'")) {
