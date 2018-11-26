e29203b5ce4b7feff3b7dc920b7c8793ab9474cb
Damien Burks <damien@Damiens-MacBook-Pro.local>
2018-11-26 16:36:29 -0600

    Password reset system

--- password-reset.php
+++ password-reset.php
@@ -7,14 +7,15 @@ if(isset($_POST) & !empty($_POST)){
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $result=mysqli_query($con, "SELECT * FROM customer WHERE Email_Address = '" . $email ."'");
     if(mysqli_num_rows($result) == 1){
-        $r = mysqli_fetch_assoc($res);
-        $password = $r['Password'];
+        $r = mysqli_fetch_assoc($result);
+        $oripass = $r['Password'];
+        $newpass = randomPassword(5,1,"lower_case,upper_case,numbers,special_symbols");
         $name = $r['FName'];
         $to = $r['Email_Address'];
         $output='<p>Dear user,</p>';
         $output.='<p>Please refer to the password that is listed below. If you need to change your password, please use the password below to login and change it.</p>';
         $output.='<p>-------------------------------------------------------------</p>';
-        $output.='<p>Password: $password</p>';		
+        $output.='<p>Password: '.$oripass.'</p>';		
         $output.='<p>-------------------------------------------------------------</p>';
         $output.='<p>Please be sure to delete this email once you have copied your password.</p>';
         $output.='<p>Thanks,</p>';
@@ -24,14 +25,14 @@ if(isset($_POST) & !empty($_POST)){
 
         $email_to = $email;
         $fromserver = "info.erpg@gmail.com"; 
-        require("PHPMailer/PHPMailerAutoload.php");
         $mail = new PHPMailer();
         $mail->IsSMTP();
         $mail->Host = "smtp.gmail.com"; // Enter your host here
         $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
         $mail->Username = "info.erpg@gmail.com"; // Enter your email here
         $mail->Password = "ERPG_Encoder1"; //Enter your password here
-        $mail->Port = 465;
+        $mail->Port = 587;
         $mail->IsHTML(true);
         $mail->From = "info.erpg@gmail.com";
         $mail->FromName = "ERPG Support Team";
@@ -45,11 +46,49 @@ if(isset($_POST) & !empty($_POST)){
         echo "<div class='error'>
         <p>An email has been sent to you with instructions on how to reset your password.</p>
         </div><br /><br /><br />";
+        $update = mysqli_query($con, "update customer set Password = md5('".$newpass."') where Password='".$oripass."' and Email_Address='".$email."'");
         }
     }
     else{
         echo "Email doesn't exist in database";
     }
+
+    //Function for Resetting Password
+    function randomPassword($length, $count, $characters) {
+ 
+        // $length - the length of the generated password
+        // $count - number of passwords to be generated
+        // $characters - types of characters to be used in the password
+         
+        // define variables used within the function    
+            $symbols = array();
+            $passwords = array();
+            $used_symbols = '';
+            $pass = '';
+         
+        // an array of different character types    
+            $symbols["lower_case"] = 'abcdefghijklmnopqrstuvwxyz';
+            $symbols["upper_case"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $symbols["numbers"] = '1234567890';
+            $symbols["special_symbols"] = '!?~@#-_+<>[]{}';
+         
+            $characters = split(",",$characters); // get characters types to be used for the passsword
+            foreach ($characters as $key=>$value) {
+                $used_symbols .= $symbols[$value]; // build a string with all characters
+            }
+            $symbols_length = strlen($used_symbols) - 1; //strlen starts from 0 so to get number of characters deduct 1
+             
+            for ($p = 0; $p < $count; $p++) {
+                $pass = '';
+                for ($i = 0; $i < $length; $i++) {
+                    $n = rand(0, $symbols_length); // get a random character from the string with all characters
+                    $pass .= $used_symbols[$n]; // add the character to the password string
+                }
+                $passwords[] = $pass;
+            }
+             
+            return $passwords; // return the generated password
+        }
 }
 ?>
 <!DOCTYPE html>
